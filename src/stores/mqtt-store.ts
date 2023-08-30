eaf3168d4fbb07c8171101dae0bfafa85a7e4d68
ph <raphaelcr.ads@gmail.com>
2023-08-30 12:39:10 -0300

    Refatoração do identificador da conexão mqtt (era utilizado o id do sensor, agora é utilizado um hash aleatório de 8 caracteres).

--- src/stores/mqtt-store.ts
+++ src/stores/mqtt-store.ts
@@ -2,19 +2,21 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { useGlobalStore } from 'stores/global-store';
 import { IClientOptions, MqttClient, connect } from 'mqtt';
+import { generateRandomHash } from 'src/helpers/random';
 import { Sensor } from './types/sensor';
 
 export const useMqttStore = defineStore('mqtt', () => {
   const globalStore = useGlobalStore();
 
-  const currentSensorId = ref<string|null>(null);
+  const currentConnectionHash = ref<string|null>(null);
   const mqttClient = ref<MqttClient | null>(null);
   const mqttIsConnected = ref<boolean>(false);
   const irrigationStatus = ref<boolean | null>(null);
   const currentHumidity = ref<number | null>(null);
 
   const connectMqtt = async (sensor: Sensor) => {
-    currentSensorId.value = sensor.id;
+    const connectionHash = generateRandomHash(8);
+    currentConnectionHash.value = connectionHash;
     const url = 'wss://97299651548a4c57a3aabb6700e6e882.s2.eu.hivemq.cloud:8884/mqtt';
     const options: IClientOptions = {
       username: 'projeto-integrador-v',
@@ -24,7 +26,7 @@ export const useMqttStore = defineStore('mqtt', () => {
 
     client.on('connect', () => {
       setTimeout(() => {
-        if (currentSensorId.value !== sensor.id) return;
+        if (currentConnectionHash.value !== connectionHash) return;
         mqttIsConnected.value = true;
       }, 1000);
     });
@@ -34,17 +36,13 @@ export const useMqttStore = defineStore('mqtt', () => {
       );
     });
     client.on('message', (topic, message) => {
-      if (currentSensorId.value !== sensor.id) return;
+      if (currentConnectionHash.value !== connectionHash) return;
       if (topic === sensor.topicIrrigationStatus) {
         irrigationStatus.value = message.toString() === 'true';
       }
       if (topic === sensor.topicCurrentHumidity) {
         currentHumidity.value = parseInt(message.toString(), 10);
       }
-    });
-    client.on('disconnect', () => {
-      // TODO: verificar a desconexão do sensor, aparentemente não está desconectando.
-      // console.log('dc', sensor.name);
     });
 
     client.subscribe(sensor.topicIrrigationStatus);
@@ -54,7 +52,7 @@ export const useMqttStore = defineStore('mqtt', () => {
   };
 
   const disconnectMqtt = async () => {
-    mqttClient.value?.end();
+    mqttClient.value?.end(true);
     mqttClient.value = null;
     mqttIsConnected.value = false;
     irrigationStatus.value = null;
